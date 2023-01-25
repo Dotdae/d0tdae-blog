26f2b7037ca2615a56b3c300115b908989102275
Christopher Ortega <chriiswho@gmail.com>
2023-01-25 16:46:11 -0700

    Schema

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import GlobalStyle from "./globalStyles"
+
+// Pages.
+
+import {Home, Projects, About} from './pages'
+
+// Partial components.
+
+function App() {
+
+  return (
+   <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route index path="/projects"  element={<Projects/>} />
+        <Route index path="/about"  element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+   </>
+  )
+}
+
+export default App
---
+++ src/pages/index.jsx
@@ -0,0 +1,24 @@
+
+export function Home(){
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export function Projects(){
+    return(
+        <div>
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export function About(){
+    return(
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
